7db312d4e3c36b0d246d750c3ffa8fb817b1ca1f
Loïc André <loic_an@protonmail.com>
2023-12-22 21:00:22 +0100

    mise a jour du 21 dec 2023

--- code/code.ino
+++ code/code.ino
@@ -1,34 +1,72 @@
 #include <SD.h>
 #include <SPI.h>
 #include "pin.hpp"
+#include <Wire.h>
+#include <LIDARLite.h>
+
+LIDARLite myLidarLite;
+Sd2Card sd;
+SdVolume vol;
+boolean isSdInit = false;
 
 void set_fan_speed(uint16_t val)
 {
-    OCR1A = (val > 320) ? 320 : val;
+  OCR1A = (val > 320) ? 320 : val;
+}
+void sd_setup()
+{
+  if (!sd.init())
+  {
+    Serial.println("sd card not initialized. Halting...");
+    while (1)
+    {
+    }
+  }
+  else
+  {
+    Serial.println("sd card initialized.");
+    isSdInit = true;
+  };
 }
 
 void setup()
 {
-    /*
-    pinMode(PIN_SONAR_TRIG, OUTPUT);
-    pinMode(PIN_MOT1_DIR, OUTPUT);
-    pinMode(PIN_MOT2_DIR, OUTPUT);
-    pinMode(PIN_MOT1_EN, OUTPUT);
-    pinMode(PIN_MOT2_EN, OUTPUT);
-    pinMode(PIN_SONAR_ECHF, INPUT);
-    pinMode(PIN_SONAR_ECHR, INPUT);
-    pinMode(PIN_SONAR_ECHL, INPUT);
-    */
+  Serial.begin(115200); // Initialize serial connection
+  Serial.println("Serial started.");
 
-    // Configuration du Timer 1 pour une fréquence PWM de 25kHz sur PIN_FAN_PWM (9)
-    TCCR1A = 0;                             // Effacer le registre TCCR1A
-    TCCR1A |= (1 << COM1A1) | (1 << WGM11); // Non inversé, mode 10 (phase correcte)
-    TCCR1B = 0;                             // Effacer le registre TCCR1B
-    TCCR1B |= (1 << WGM13) | (1 << CS10);   // Mode 10 (phase correcte), pas de pré-diviseur
-    ICR1 = 320;                             // Valeur TOP pour une fréquence de 25kHz
-    pinMode(PIN_FAN_PWM, OUTPUT);           // Mettre le pin 9 en sortie
-    set_fan_speed(320);
+  sd_setup();
+  myLidarLite.begin(1, true);
+  Serial.println("Lidar started.");
+
+  pinMode(PIN_SONAR_TRIG, OUTPUT);
+  pinMode(PIN_MOT1_DIR1, OUTPUT);
+  pinMode(PIN_MOT2_DIR1, OUTPUT);
+  pinMode(PIN_MOT1_DIR2, OUTPUT);
+  pinMode(PIN_MOT2_DIR2, OUTPUT);
+  pinMode(PIN_MOT1_EN, OUTPUT);
+  pinMode(PIN_MOT2_EN, OUTPUT);
+  pinMode(PIN_SONAR_ECHU, INPUT);
+  pinMode(PIN_SONAR_ECHR, INPUT);
+  pinMode(PIN_SONAR_ECHL, INPUT);
+
+  // Configuration du Timer 1 pour une fréquence PWM de 25kHz sur PIN_FAN_PWM (9)
+  TCCR1A = 0;                             // Effacer le registre TCCR1A
+  TCCR1A |= (1 << COM1A1) | (1 << WGM11); // Non inversé, mode 10 (phase correcte)
+  TCCR1B = 0;                             // Effacer le registre TCCR1B
+  TCCR1B |= (1 << WGM13) | (1 << CS10);   // Mode 10 (phase correcte), pas de pré-diviseur
+  ICR1 = 320;                             // Valeur TOP pour une fréquence de 25kHz
+  pinMode(PIN_FAN_PWM, OUTPUT);           // Mettre le pin 9 en sortie
+  set_fan_speed(320);
+  Serial.println("FAN PWM setup done.");
+  Serial.println("PINs setup done.");
+
+  digitalWrite(PIN_MOT1_DIR2, HIGH);
+  digitalWrite(PIN_MOT2_DIR2, HIGH);
+
+  Serial.println("SETUP DONE. ENTERING LOOP...");
 }
+
 void loop()
 {
+  Serial.println(myLidarLite.distance());
 }
--- code/pin.hpp
+++ code/pin.hpp
@@ -1,13 +1,15 @@
-#define PIN_SD_MISO 13
-#define PIN_SD_SCK 12
-#define PIN_SD_MOSI 11
-#define PIN_SD_CS 10
+// #define PIN_SD_MISO 13 pin par defaut, inutile de define
+// #define PIN_SD_SCK 12
+// #define PIN_SD_MOSI 11
+// #define PIN_SD_CS 10
 #define PIN_FAN_PWM 9
 #define PIN_SONAR_TRIG 8
-#define PIN_MOT1_DIR 7
+#define PIN_MOT1_DIR1 7
 #define PIN_MOT1_EN 6
 #define PIN_MOT2_EN 5
-#define PIN_MOT2_DIR 4
-#define PIN_SONAR_ECHF A0
+#define PIN_MOT1_DIR2 4
+#define PIN_MOT2_DIR1 3
+#define PIN_MOT2_DIR2 2
+#define PIN_SONAR_ECHU A0
 #define PIN_SONAR_ECHR A1
 #define PIN_SONAR_ECHL A2
